e82416c06a262fcc54ac1a078e34388dbce7261c
Mathias Dolidon <mathias.dolidon@gmail.com>
2014-09-03 14:29:09 +0200

    Test multiple files processing

--- test/runtest.sh
+++ test/runtest.sh
@@ -12,8 +12,7 @@ rm *test 2>/dev/null
 
 echo "Round tripping in files mode unix -> oldmac -> win -> unix"
 
-cp unixref unixtest
-cp unixtest oldmactest
+cp unixref oldmactest
 ../endlines oldmac oldmactest &>/dev/null
 
 cp oldmactest wintest
@@ -31,21 +30,21 @@ WINTEST=`$MD5<wintest`
 OLDMACTEST=`$MD5<oldmactest`
 
 
-if [ "$OLDMACREF" == "$OLDMACTEST" ]
+if [[ "$OLDMACREF" == "$OLDMACTEST" ]]
 then
     echo "OK : oldmac output"
 else
     echo "FAILURE : oldmac output doesn't match"
 fi
 
-if [ "$WINREF" == "$WINTEST" ]
+if [[ "$WINREF" == "$WINTEST" ]]
 then
     echo "OK : windows output"
 else
     echo "FAILURE : windows output doesn't match"
 fi
 
-if [ "$UNIXREF" == "$UNIXTEST" ]
+if [[ "$UNIXREF" == "$UNIXTEST" ]]
 then
     echo "OK : unix output"
 else
@@ -55,20 +54,19 @@ fi
 # Part 2 : large files, testing the buffers' integrity
 
 echo "...building a large file..."
-echo "...processing the large file..."
-
 for ((i=1;i<=500;i++));
 do
     cat unixref >> bigunixintest
 done
 
+echo "...processing the large file..."
 ../endlines win <bigunixintest 2>/dev/null | ../endlines oldmac 2>/dev/null | ../endlines unix >bigunixouttest 2>/dev/null
 
 
 BIGUNIXIN=`$MD5<bigunixintest`
 BIGUNIXOUT=`$MD5<bigunixouttest`
 
-if [ "$BIGUNIXIN" == "$BIGUNIXOUT" ]
+if [[ "$BIGUNIXIN" == "$BIGUNIXOUT" ]]
 then
     echo "OK : large file processing"
 else
@@ -83,22 +81,35 @@ echo "" | ../endlines dunnothis 2>unknowntest >/dev/null
 UNKNOWN=`cat unknowntest`
 if [[ $UNKNOWN == *unknown* ]]
 then
-      echo "OK : reacts to an unknown convention";
+      echo "OK : reacts to an unknown convention"
 else
     echo "FAILURE : failed to react to an unknown convention"
 fi
 
-echo "" | ../endlines lf -q 2>quiettest >/dev/null
+echo "" | ../endlines unix -q 2>quiettest >/dev/null
 QUIET=`cat quiettest`
 if [[ -z $QUIET ]]
 then
-    echo "OK : quietens down with -q";
+    echo "OK : quietens down with -q"
 else
     echo "FAILURE : keeps chatting even though -q was used"
 fi
 
+mkdir dummydir
+cp unixref multi1test
+cp unixref multi2test
+cp unixref multi3test
+../endlines win -q dummydir multi1test multi2test multi3test
 
-
-
+MULTIONE=`$MD5<multi1test`
+MULTITWO=`$MD5<multi2test`
+MULTITHREE=`$MD5<multi3test`
+if [[ "$MULTIONE" == "$WINREF" &&  "$MULTITWO" == "$WINREF" && "$MULTITHREE" == "$WINREF" ]]
+then
+    echo "OK : multiple files"
+else
+    echo "FAILURE : failure to handle multiple files"
+fi
+rmdir dummydir
 
 rm *test
