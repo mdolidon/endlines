6c49608edcee1a6573ae3b7ca52185f5a8af54ea
Mathias Dolidon <mathias.dolidon@gmail.com>
2016-11-06 12:08:21 +0100

    a few test cases for -r option

--- test/cases/multiple_files.sh
+++ test/cases/multiple_files.sh
@@ -20,3 +20,84 @@ else
     ./case_failed.sh
 fi
 rmdir dummydir
+
+
+
+
+
+mkdir sandbox/subdir1
+mkdir sandbox/subdir1/subdir11
+mkdir sandbox/subdir1/subdir12
+mkdir sandbox/subdir1/.hidden_subdir
+
+cp data/unixref sandbox/subdir1/file1a
+cp data/unixref sandbox/subdir1/file1b
+cp data/unixref sandbox/subdir1/subdir11/file111a
+cp data/unixref sandbox/subdir1/subdir11/file111b
+cp data/unixref sandbox/subdir1/subdir12/file112a
+cp data/unixref sandbox/subdir1/subdir12/file112b
+cp data/unixref sandbox/subdir1/.hidden_subdir/file_inside_hidden
+cp data/unixref sandbox/subdir1/.hidden_file
+
+
+$ENDLINES win -r sandbox/subdir1 &>/dev/null
+
+UNIXREF=`$MD5<data/unixref`
+FILE1A=`$MD5<sandbox/subdir1/file1a`
+FILE1B=`$MD5<sandbox/subdir1/file1b`
+FILE111A=`$MD5<sandbox/subdir1/subdir11/file111a`
+FILE111B=`$MD5<sandbox/subdir1/subdir11/file111b`
+FILE112A=`$MD5<sandbox/subdir1/subdir12/file112a`
+FILE112B=`$MD5<sandbox/subdir1/subdir12/file112b`
+
+
+if [[ 
+    "$WINREF" == "$FILE1A" &&
+    "$WINREF" == "$FILE1B" &&
+    "$WINREF" == "$FILE111A" &&
+    "$WINREF" == "$FILE111B" &&
+    "$WINREF" == "$FILE112A" &&
+    "$WINREF" == "$FILE112B" 
+]]
+then 
+    echo "OK : finding regular files in subdirectories with -r"
+else
+    echo "FAILURE : regular files in subdirectories were not found when using -r"
+    ./case_failed.sh
+fi
+
+FILE_INSIDE_HIDDEN=`$MD5<sandbox/subdir1/.hidden_subdir/file_inside_hidden`
+HIDDEN_FILE=`$MD5<sandbox/subdir1/.hidden_file`
+
+
+if [[ 
+    "$UNIXREF" == "$FILE_INSIDE_HIDDEN" &&
+    "$UNIXREF" == "$HIDDEN_FILE"
+]]
+then
+    echo "OK : hidden files and directories were not affected by -r"
+else
+    echo "FAILURE : hidden files and directories changed by -r even without -h"
+    ./case_failed.sh
+fi
+
+
+
+$ENDLINES win -r -h sandbox/subdir1 &>/dev/null
+
+FILE_INSIDE_HIDDEN=`$MD5<sandbox/subdir1/.hidden_subdir/file_inside_hidden`
+HIDDEN_FILE=`$MD5<sandbox/subdir1/.hidden_file`
+
+if [[ 
+    "$WINREF" == "$FILE_INSIDE_HIDDEN" &&
+    "$WINREF" == "$HIDDEN_FILE"
+]]
+then
+    echo "OK : hidden files and directories processed with -r -h"
+else
+    echo "FAILURE : hidden files and directories not changed by -r even with -h"
+    ./case_failed.sh
+fi
+
+
+rm -rf sandbox/subdir1
